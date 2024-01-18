eff4359124d25f4aa03e4bf8d871b13eb162f63f
AntonyChR <antonychr99@gmail.com>
2024-01-18 14:37:40 -0500

    add: norm, projection, add negatinve

--- src/vector.ts
+++ src/vector.ts
@@ -1,8 +1,7 @@
-
 export default class Vector {
     public x: number;
     public y: number;
-    constructor(x: number, y:number){
+    constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
     }
@@ -10,16 +9,36 @@ export default class Vector {
     add(other: Vector): Vector {
         return new Vector(this.x + other.x, this.y + other.y);
     }
+    
+    minus(other: Vector){
+        return new Vector(this.x - other.x, this.y - other.y);
+    }
 
     scalarProduct(scalar: number): Vector {
         return new Vector(this.x * scalar, this.y * scalar);
     }
 
-    crossProduct(other: Vector): Vector {
-        return new Vector(this.x * other.y - this.y * other.x, this.x * other.y + this.y * other.x);
+    dot(other: Vector) {
+        return this.x * other.x + this.y * other.y;
     }
 
-    norm(): number {
+    crossProduct(other: Vector): Vector {
+        return new Vector(
+            this.x * other.y - this.y * other.x,
+            this.x * other.y + this.y * other.x
+        );
+    }
+
+    norm(other?: Vector): number {
+        if (other) {
+            let t = this.add(other.scalarProduct(-1)); // norm(this - other)
+            return Math.sqrt(t.x ** 2 + t.y ** 2);
+        }
         return Math.sqrt(this.x ** 2 + this.y ** 2);
+    }
+
+    projectedOnto(other: Vector){
+        let sc = this.dot(other) / other.dot(other)
+        return other.scalarProduct(sc)
     }
 }
