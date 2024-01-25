c5c52032896ab06684ea2448c38c8777b8f4f9f8
AntonyChR <antonychr99@gmail.com>
2024-01-25 16:19:55 -0500

    add: line color

--- src/forms/Form.ts
+++ src/forms/Form.ts
@@ -2,5 +2,5 @@ export default abstract class Form {
     abstract ctx: CanvasRenderingContext2D;
     abstract width: number;
     abstract draw(): void;
-    abstract color: string;
+    abstract lineColor: string;
 }
