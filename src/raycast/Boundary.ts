29094e2f698f0e422df80304f5355914ea92b5ed
AntonyChR <antonychr99@gmail.com>
2024-01-30 17:44:39 -0500

    add: color lines

--- src/raycast/Boundary.ts
+++ src/raycast/Boundary.ts
@@ -1,17 +1,26 @@
-import { Line, NewLine } from "../forms/Line";
-import Vector from "../vector/vector";
+import { Line, NewLine } from '../forms/Line';
+import Vector from '../vector/vector';
 
 export class Boundary {
-    public a: Vector
-    public b: Vector
-    public line: Line
-    constructor(x1:number,y1:number,x2:number,y2:number, canvasCtx: CanvasRenderingContext2D){
-        this.a = new Vector(x1,y1)
-        this.b = new Vector(x2,y2)
-        this.line = NewLine(this.a,this.b,canvasCtx)        
+    public a: Vector;
+    public b: Vector;
+    public line: Line;
+    public color: string;
+    constructor(
+        x1: number,
+        y1: number,
+        x2: number,
+        y2: number,
+        canvasCtx: CanvasRenderingContext2D,
+        color: string = '#404040'
+    ) {
+        this.a = new Vector(x1, y1);
+        this.b = new Vector(x2, y2);
+        this.color = color;
+        this.line = NewLine(this.a, this.b, canvasCtx, 7, this.color);
     }
 
-    draw(){
-        this.line.draw()
+    draw() {
+        this.line.draw();
     }
 }
