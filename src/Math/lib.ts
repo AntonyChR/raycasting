fe76cb24bbd3acf2e03e1244d02369a00d5613aa
AntonyChR <antonychr99@gmail.com>
2024-01-31 22:42:55 -0500

    add: normalization function

--- src/Math/lib.ts
+++ src/Math/lib.ts
@@ -1,3 +1,7 @@
 export const toRadians = (angle: number) => {
     return (angle / 180) * Math.PI;
 };
+
+export function normalize(value: number, rmin: number, rmax: number, tmin: number, tmax: number): number {
+    return ((value - rmin) / (rmax - rmin)) * (tmax - tmin) + tmin;
+}
