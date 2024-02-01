6c78bfd91bebd5f589d605aec400ae4dcdf211b6
AntonyChR <antonychr99@gmail.com>
2024-01-31 22:55:50 -0500

    add: documentation

--- src/Math/lib.ts
+++ src/Math/lib.ts
@@ -2,6 +2,17 @@ export const toRadians = (angle: number) => {
     return (angle / 180) * Math.PI;
 };
 
+/**
+ * Normalizes a value within a specific range and scales it to another desired range.
+ *
+ * @param value - The value to normalize.
+ * @param rmin - The minimum value of the original range.
+ * @param rmax - The maximum value of the original range.
+ * @param tmin - The minimum value of the new desired range.
+ * @param tmax - The maximum value of the new desired range.
+ * @returns The normalized value adjusted to the new range.
+ */
+
 export function normalize(
     value: number,
     rmin: number,
